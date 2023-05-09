694f022430dfe349cdfeee06b0fd26a6a52d342b
keycodemodo <1324506445@qq.com>
2023-05-09 20:27:25 +0800

    登录验证JSON格式数据返回

---
+++ study-proj-backend/src/main/java/com/example/StudyProjBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjBackendApplication.class, args);
+    }
+
+}
---
+++ study-proj-backend/src/main/java/com/example/entity/Restbean.java
@@ -0,0 +1,30 @@
+package com.example.entity;
+
+import lombok.Data;
+
+@Data
+public class Restbean<T> {
+    private int status;
+    private boolean success;
+    private T Message;
+
+    private Restbean(int status, boolean success, T message) {
+        this.status = status;
+        this.success = success;
+        Message = message;
+    }
+
+    public static <T> Restbean<T> success(){
+        return new Restbean<>(200,true,null);
+    }
+
+    public static <T> Restbean<T> success(T data){
+        return new Restbean<>(200,true,data);
+    }
+    public static <T> Restbean<T> failure(int status){
+        return new Restbean<>(status,false,null);
+    }
+    public static <T> Restbean<T> failure(int status,T data){
+        return new Restbean<>(status,false,data);
+    }
+}
